840fa578c174bcc5273d4cdf6ae7b968bbf6c8a5
Matej Capkovic <mcapkovic@gmail.com>
2023-12-03 20:30:45 +0100

    day3p2

--- src/day03/index.ts
+++ src/day03/index.ts
@@ -13,17 +13,6 @@ const example1 = `467..114..
 ...$.*....
 .664.598..`;
 
-const example2 = `467..114..
-...*......
-..35..633.
-......#...
-617*......
-.....+.53.
-..592....3
-...755....
-...$.*....
-.664.598..`;
-
 // ---------------------------- solution ----------------------------
 
 const parseInput = (rawInput: string) => rawInput.split("\n");
@@ -40,7 +29,26 @@ const ADJACENT = [
   [1, 1], // bottom right
 ];
 
-function hasSymbolAround({ column, row, input }) {
+const ALL_ADJACENT = [
+  [-1, -1], // top left
+  [-1, 0], // top
+  [-1, 1], // top right
+  [0, -1], // left
+  [0, 1], // right
+  [1, -1], // bottom left
+  [1, 0], // bottom
+  [1, 1], // bottom right
+];
+
+function hasSymbolAround({
+  column,
+  row,
+  input,
+}: {
+  column: number;
+  row: number;
+  input: string[];
+}) {
   // check left
   const leftSymbol = input[row][column - 1] ?? ".";
   const isLeftSymbolNumber = !isNaN(Number(leftSymbol));
@@ -67,11 +75,8 @@ function hasSymbolAround({ column, row, input }) {
 }
 
 const part1 = (rawInput: string) => {
-  console.log("rawInput", rawInput);
   const input = parseInput(rawInput);
-  console.log(input);
-
-  const partNumbers = [];
+  const partNumbers: number[] = [];
 
   input.forEach((row, rowIndex) => {
     let potentialNumber = "";
@@ -102,15 +107,97 @@ const part1 = (rawInput: string) => {
     }
   });
 
-  console.log(partNumbers);
-
   return partNumbers.reduce((acc, number) => acc + number, 0);
 };
+
+function getGearAround({
+  column,
+  row,
+  input,
+}: {
+  column: number;
+  row: number;
+  input: string[];
+}) {
+  const gearsAround: string[] = [];
+
+  ALL_ADJACENT.forEach(([rowOffset, colOffset]) => {
+    const adjacentRow = row + rowOffset;
+    const adjacentCol = column + colOffset;
+
+    if (input[adjacentRow] == null) return;
+    if (input[adjacentRow][adjacentCol] == null) return;
+
+    const adjacentSymbol = input[adjacentRow][adjacentCol];
+    if (adjacentSymbol != "*") return;
+
+    gearsAround.push(`${adjacentRow}-${adjacentCol}`);
+  });
+
+  return gearsAround;
+}
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  return;
+  const partNumbers: {
+    value: number;
+    gearsAround: string[];
+  }[] = [];
+
+  input.forEach((row, rowIndex) => {
+    let potentialNumber = "";
+    const gearsAroundPotentialNumber = new Set<string>();
+
+    for (let columnIndex = 0; columnIndex < row.length; columnIndex++) {
+      const currentSymbol = row[columnIndex];
+      const isNumber = !isNaN(Number(currentSymbol));
+
+      if (isNumber) {
+        potentialNumber += currentSymbol;
+
+        const gearsAround = getGearAround({
+          column: columnIndex,
+          row: rowIndex,
+          input,
+        });
+
+        gearsAround.forEach((gear) => gearsAroundPotentialNumber.add(gear));
+
+        if (columnIndex != row.length - 1) continue; // continue if not last symbol
+      }
+
+      // non number
+      if (gearsAroundPotentialNumber.size > 0 && potentialNumber.length > 0) {
+        partNumbers.push({
+          value: Number(potentialNumber),
+          gearsAround: [...gearsAroundPotentialNumber],
+        });
+      }
+      potentialNumber = "";
+      gearsAroundPotentialNumber.clear();
+    }
+  });
+
+  const gears = new Map<string, number[]>();
+  partNumbers.forEach(({ value, gearsAround }) => {
+    gearsAround.forEach((gear) => {
+      const currentGearValue = gears.get(gear) ?? [];
+      currentGearValue.push(value);
+      gears.set(gear, currentGearValue);
+    });
+  });
+
+  let sum = 0;
+  gears.forEach((values, gear) => {
+    if (values.length === 2) {
+      const [firstValue, secondValue] = values;
+      const ratio = firstValue * secondValue;
+      sum += ratio;
+    }
+  });
+
+  return sum;
 };
 
 // ---------------------------- config ----------------------------
@@ -118,7 +205,7 @@ const part2 = (rawInput: string) => {
 const part1Config = {
   tests: [
     {
-      input: example2,
+      input: example1,
       expected: 4361,
     },
   ],
@@ -129,7 +216,7 @@ const part2Config = {
   tests: [
     {
       input: example1,
-      expected: "",
+      expected: 467835,
     },
   ],
   solution: part2,
@@ -137,7 +224,7 @@ const part2Config = {
 
 run({
   part1: part1Config,
-  // part2: part2Config,
+  part2: part2Config,
   trimTestInputs: true,
   // onlyTests: true,
   onlyTests: false,
