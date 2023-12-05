20f3f6caee112f7d49dd9663a14d1c9db069952d
Matej Capkovic <mcapkovic@gmail.com>
2023-12-05 18:51:26 +0100

    day4p2

--- src/day04/index.ts
+++ src/day04/index.ts
@@ -19,7 +19,7 @@ const parseInput = (rawInput: string) =>
     const winningNumbers = winningValues.match(/\d+/g)?.map(Number) ?? [];
     const myNumbers = myValues.match(/\d+/g)?.map(Number) ?? [];
     return {
-      card: cardNumber,
+      cardNumber: cardNumber,
       winningValues: winningNumbers,
       myValues: myNumbers,
     };
@@ -47,10 +47,161 @@ const part1 = (rawInput: string) => {
   return cardsWorth.reduce((acc, value) => acc + value, 0);
 };
 
+const getNoOfWinningValues = (winningValues: number[], myValues: number[]) => {
+  let noOfWinningValues = 0;
+
+  myValues.forEach((myValue) => {
+    if (winningValues.includes(myValue)) {
+      noOfWinningValues++;
+    }
+  });
+
+  return noOfWinningValues;
+};
+
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
+  // console.log(input);
 
-  return;
+  const cards = [...input];
+
+  let counter = 0;
+  do {
+    counter++;
+    if (counter % 10000 === 0) console.log("counter", counter);
+    const card = cards.shift();
+    if (!card) break;
+
+    const { winningValues, myValues, cardNumber } = card;
+    // console.log('cardNumber', cardNumber)
+    let noOfWinningValues = 0;
+
+    noOfWinningValues = getNoOfWinningValues(winningValues, myValues);
+    const newCards = input.slice(cardNumber, cardNumber + noOfWinningValues);
+    newCards.forEach((newCard) => cards.push(newCard));
+  } while (cards.length > 0);
+
+  console.log("counter", counter);
+  return counter;
+};
+
+const part2v2 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+  // console.log(input);
+
+  const cards = [...input];
+  const cardsWinCounts = new Map();
+
+  let counter = 0;
+  do {
+    counter++;
+    if (counter % 10000 === 0) console.log("counter", counter);
+    const card = cards.shift();
+    if (!card) break;
+
+    const { winningValues, myValues, cardNumber } = card;
+    // console.log('cardNumber', cardNumber)
+    let noOfWinningValues = 0;
+
+    if (cardsWinCounts.has(cardNumber)) {
+      noOfWinningValues = cardsWinCounts.get(cardNumber);
+    } else {
+      noOfWinningValues = getNoOfWinningValues(winningValues, myValues);
+      cardsWinCounts.set(cardNumber, noOfWinningValues);
+    }
+
+    // noOfWinningValues = getNoOfWinningValues(winningValues, myValues);
+    const newCards = input.slice(cardNumber, cardNumber + noOfWinningValues);
+    newCards.forEach((newCard) => cards.push(newCard));
+  } while (cards.length > 0);
+
+  console.log("counter", counter);
+  return counter;
+};
+
+const part2v3 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+  // console.log(input);
+
+  const cards = [...input];
+  const winningCards = new Map();
+
+  let counter = 0;
+  do {
+    counter++;
+    if (counter % 10000 === 0) console.log("counter", counter);
+    const card = cards.shift();
+    if (!card) break;
+
+    const { winningValues, myValues, cardNumber } = card;
+    // console.log('cardNumber', cardNumber)
+    let newCards = [];
+
+    if (winningCards.has(cardNumber)) {
+      newCards = winningCards.get(cardNumber);
+    } else {
+      const noOfWinningValues = getNoOfWinningValues(winningValues, myValues);
+      newCards = input.slice(cardNumber, cardNumber + noOfWinningValues);
+      winningCards.set(cardNumber, newCards);
+    }
+
+    newCards.forEach((newCard) => cards.push(newCard));
+  } while (cards.length > 0);
+
+  console.log("counter", counter);
+  return counter;
+};
+
+const part2v4 = (rawInput: string) => {
+  const input = parseInput(rawInput);
+  // console.log(input);
+
+  const cardsData = new Map();
+
+  input.forEach((card) => {
+    const { winningValues, myValues, cardNumber } = card;
+    const noOfWinningValues = getNoOfWinningValues(winningValues, myValues);
+
+    const newCards = input.slice(cardNumber, cardNumber + noOfWinningValues);
+
+    cardsData.set(cardNumber, {
+      cardNumber,
+      newCards: newCards.map((newCard) => newCard.cardNumber),
+    });
+  });
+
+  let count = 0;
+  const remainingCards = [...input];
+
+  // count cards from back to front
+  do {
+    count++;
+    const currentCard = remainingCards.pop();
+    if (!currentCard) break;
+
+    const { cardNumber } = currentCard;
+    const { newCards, points: mainCardPoints } = cardsData.get(cardNumber);
+
+    if (mainCardPoints != null) {
+      count = count + mainCardPoints;
+    } else {
+      // get points from cards under (won cards)
+      const cardsUnderPoints = newCards.map((newCardNumber) => {
+        const { points: wonCardPoints } = cardsData.get(newCardNumber);
+        return (wonCardPoints ?? 0) + 1; // +1 for the card itself
+      });
+
+      const points = cardsUnderPoints.reduce((acc, value) => acc + value, 0);
+      count = count + points;
+      cardsData.set(cardNumber, {
+        cardNumber,
+        newCards,
+        points,
+      });
+    }
+  } while (remainingCards.length > 0);
+
+  return count;
 };
 
 // ---------------------------- config ----------------------------
@@ -69,16 +220,17 @@ const part2Config = {
   tests: [
     {
       input: example1,
-      expected: 13,
+      expected: 30,
     },
   ],
-  solution: part2,
+  solution: part2v4,
 };
 
 run({
   part1: part1Config,
-  // part2: part2Config,
+  part2: part2Config,
   trimTestInputs: true,
   // onlyTests: true,
   onlyTests: false,
 });
+
