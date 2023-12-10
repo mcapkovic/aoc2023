0ed2bae398cb37a245b9a96d1fd4dd6af5356482
Matej Capkovic <mcapkovic@gmail.com>
2023-12-10 09:14:02 +0100

    add day10p2

--- src/day10/index.ts
+++ src/day10/index.ts
@@ -14,6 +14,16 @@ SJ.L7
 |F--J
 LJ...`;
 
+const example3 = `...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+...........`;
+
 // ---------------------------- solution ----------------------------
 
 const parseInput = (rawInput: string) => rawInput.split("\n");
@@ -85,11 +95,8 @@ function findStart(input: string[]): [number, number] {
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
-  console.log(input);
 
   const start = findStart(input);
-  console.log(start);
-
   let pastLocation = start;
   let currentLocation = start;
   let stepCounter = 0;
@@ -111,15 +118,13 @@ const part1 = (rawInput: string) => {
 
   do {
     const currentSymbol = input[currentLocation[0]][currentLocation[1]];
-    if(currentSymbol === 'S') break;
-    // console.log(currentSymbol);
+    if (currentSymbol === "S") break;
+
     const increments = directions.get(currentSymbol);
     const nextPossibleLocations = increments.map((increment) => [
       currentLocation[0] + increment[0],
       currentLocation[1] + increment[1],
     ]);
-    // console.log('pastLocation', pastLocation)
-    // console.log('nextPossibleLocations', nextPossibleLocations);
 
     let possibleLocation = nextPossibleLocations[0];
     if (
@@ -128,8 +133,6 @@ const part1 = (rawInput: string) => {
     )
       possibleLocation = nextPossibleLocations[1];
 
-    // console.log('possibleLocation', possibleLocation)
-  
     pastLocation = currentLocation;
     currentLocation = possibleLocation;
 
@@ -142,10 +145,132 @@ const part1 = (rawInput: string) => {
   return stepCounter / 2;
 };
 
+function firstMove(input: string[], start: [number, number]) {
+  let currentLocation = start;
+
+  const top = input[start[0] - 1][start[1]];
+  const left = input[start[0]][start[1] - 1];
+  const right = input[start[0]][start[1] + 1];
+  const bottom = input[start[0] + 1][start[1]];
+
+  if (top === "|" || top === "7" || top === "F")
+    currentLocation = [start[0] - 1, start[1]];
+  if (left === "-" || left === "J" || left === "7")
+    currentLocation = [start[0], start[1] - 1];
+  if (right === "-" || right === "F" || right === "L")
+    currentLocation = [start[0], start[1] + 1];
+  if (bottom === "|" || bottom === "L" || bottom === "J")
+    currentLocation = [start[0] + 1, start[1]];
+
+  return currentLocation;
+}
+
+// Ray casting algorithm
+// https://www.youtube.com/watch?v=RSXM9bgqxJM
+function isInside(edges: [number, number][], xp: number, yp: number) {
+  let cnt = 0;
+  for (let index = 0; index < edges.length; index++) {
+    const edge = edges[index];
+    const [x1, y1] = edge[0];
+    const [x2, y2] = edge[1];
+    if (yp < y1 !== yp < y2 && xp < x1 + ((yp - y1) / (y2 - y1)) * (x2 - x1)) {
+      cnt += 1;
+    }
+  }
+  return cnt % 2 === 1;
+}
+
+function isVertex(currentSymbol: string) {
+  if (
+    currentSymbol === "L" ||
+    currentSymbol === "J" ||
+    currentSymbol === "7" ||
+    currentSymbol === "F"
+  )
+    return true;
+  return false;
+}
+
+function isSameAsPastLocation(
+  possibleLocation: [number, number],
+  pastLocation: [number, number],
+) {
+  if (
+    possibleLocation[0] === pastLocation[0] &&
+    possibleLocation[1] === pastLocation[1]
+  )
+    return true;
+  return false;
+}
+
+// convert [x, y] to x-y string
+function convertToString({ row, col }: { row: number; col: number }) {
+  return `${row}-${col}`;
+}
+
+
+
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
+  const start = findStart(input);
+  const visited = new Set<string>();
+  const allVertices: [number, number][] = [];
+  let pastLocation = start;
+  let currentLocation = start;
 
-  return;
+  // calculate first step manually
+  allVertices.push(currentLocation);
+  visited.add(
+    convertToString({ row: currentLocation[0], col: currentLocation[1] }),
+  );
+  currentLocation = firstMove(input, start);
+
+  // calculate the rest of the steps
+  do {
+    visited.add(
+      convertToString({ row: currentLocation[0], col: currentLocation[1] }),
+    );
+    const currentSymbol = input[currentLocation[0]][currentLocation[1]];
+    if (isVertex(currentSymbol)) allVertices.push(currentLocation);
+
+    if (currentSymbol === "S") break;
+
+    const increments = directions.get(currentSymbol);
+    const nextPossibleLocations = increments.map((increment) => [
+      currentLocation[0] + increment[0],
+      currentLocation[1] + increment[1],
+    ]);
+
+    let possibleLocation = nextPossibleLocations[0];
+    if (isSameAsPastLocation(possibleLocation, pastLocation))
+      possibleLocation = nextPossibleLocations[1];
+
+    pastLocation = currentLocation;
+    currentLocation = possibleLocation;
+
+  } while (true);
+
+  const edges = allVertices.map((vertex, index) => [
+    vertex,
+    allVertices[(index + 1) % allVertices.length],
+  ]);
+
+  const noOfRows = input.length;
+  const noOfCols = input[0].length;
+  let insidePoints = 0;
+
+  // check all points inside the grid but ignore visited points
+  for (let row = 0; row < noOfRows; row++) {
+    for (let col = 0; col < noOfCols; col++) {
+      const wasVisited = visited.has(convertToString({ row, col }));
+      if (wasVisited) continue;
+
+      const isPointInside = isInside(edges, row, col);
+      if (isPointInside) insidePoints++;
+    }
+  }
+
+  return insidePoints;
 };
 
 // ---------------------------- config ----------------------------
@@ -163,17 +288,17 @@ const part1Config = {
 const part2Config = {
   tests: [
     {
-      input: example1,
-      expected: "",
+      input: example3,
+      expected: 4,
     },
   ],
   solution: part2,
 };
 
 run({
-  part1: part1Config,
-  // part2: part2Config,
+  // part1: part1Config,
+  part2: part2Config,
   trimTestInputs: true,
-  onlyTests: true,
-  // onlyTests: false,
+  // onlyTests: true,
+  onlyTests: false,
 });
